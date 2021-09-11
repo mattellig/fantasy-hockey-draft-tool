e1a547d00feed7a58bd19cf544482b919955b29a
Matt Ellig <matt.ellig@hey.com>
2021-09-11 16:06:45 -0400

    Add build tooling

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import './index.css';
+
+ReactDOM.render(
+    <React.StrictMode>
+        <p>Hello, world!</p>
+    </React.StrictMode>,
+    document.getElementById('root')
+);

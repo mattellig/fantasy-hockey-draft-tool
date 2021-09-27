9ae684a0b7155928df93266b6795cfbfdc7c86b4
Matt Ellig <matt.ellig@hey.com>
2021-09-27 16:06:44 -0400

    Add button destructive state

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -4,6 +4,7 @@ import Spinner from '../Spinner/Spinner';
 
 interface ButtonProps {
     children?: string | string[];
+    destructive?: boolean;
     disabled?: boolean;
     icon?: React.ReactNode;
     link?: boolean;
@@ -13,9 +14,31 @@ interface ButtonProps {
     submit?: boolean;
 }
 
+const getButtonStyles = (loading: boolean, link: boolean, destructive: boolean, primary: boolean) => {
+    let colorClasses = '';
+
+    if (primary) {
+        colorClasses = 'bg-blue-600 text-white hover:bg-blue-500 active:bg-blue-700 focus:ring-blue-300';
+    } else if (destructive) {
+        colorClasses = 'bg-red-600 text-white hover:bg-red-500 active:bg-red-700 focus:ring-red-300';
+    } else if (link) {
+        colorClasses = 'text-blue-600 hover:text-blue-800 disabled:text-gray-700 focus:ring-blue-500';
+    } else {
+        colorClasses = 'bg-gray-200 text-gray-700 hover:bg-gray-100 active:bg-gray-300 focus:ring-gray-400';
+    }
+
+    return clsx(
+        'relative inline-flex items-center justify-center rounded-md text-sm font-medium focus:outline-none disabled:opacity-50 disabled:cursor-not-allowed transition',
+        !link ? 'px-2.5 py-1.5 focus:ring' : 'focus:ring-2',
+        colorClasses,
+        loading && 'opacity-75 pointer-events-none',
+    );
+};
+
 const Button = (props: ButtonProps): JSX.Element => {
     const {
         children,
+        destructive = false,
         disabled = false,
         icon,
         link = false,
@@ -25,14 +48,7 @@ const Button = (props: ButtonProps): JSX.Element => {
         submit = false,
     } = props;
 
-    const styles = link ? clsx(
-        'relative inline-flex items-center justify-center rounded text-sm font-medium text-blue-600 hover:text-blue-800 focus:outline-none focus:ring-2 focus:ring-blue-500 transition disabled:text-gray-700 disabled:opacity-50 disabled:cursor-not-allowed',
-        loading && 'opacity-75 pointer-events-none',
-    ) : clsx(
-        'relative inline-flex items-center justify-center px-2.5 py-1.5 rounded-md border border-transparent text-sm font-medium focus:outline-none focus:ring transition disabled:opacity-50 disabled:cursor-not-allowed',
-        primary ? 'bg-blue-600 text-white hover:bg-blue-500 active:bg-blue-700 focus:ring-blue-300' : 'bg-gray-200 text-gray-700 hover:bg-gray-100 active:bg-gray-300 focus:ring-gray-400',
-        loading && 'opacity-75 pointer-events-none',
-    );
+    const styles = getButtonStyles(loading, link, destructive, primary);
 
     return (
         <button
